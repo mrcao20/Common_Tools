046ee1cf51220e92ed3fc972fa53e9cf70d2febe
mrcao20 <839566521@qq.com>
2019-05-08 14:02:31 +0800

    Update McFileSlicing.h

--- C++/McFileSlicing.h
+++ C++/McFileSlicing.h
@@ -12,8 +12,16 @@ public:
 	~McFileSlicing();
 
 	bool fileSlicing(const QString &inFilePath, const QString &outFilePath, int segmentTime, bool isDeleteSource = false);
+	bool fileSlicing_new(const QString &inFilePath, const QString &outFilePath, int segmentTime, bool isDeleteSource = false);
+	bool truncateAndSlicing(const QString &inFilePath, const QString &outFilePath, int segmentTime, bool isDeleteSource = false);
+
+signals:
+	void slicingFinished();
+	void errorOccurred();
 
 private:
 	bool isH264AndAac(const QString &filePath);
 	void completeM3u8(const QString &tempFilePath, const QString &m3u8FilePath);
+	bool del_File(const QString &path);
+	bool checkoutDir(const QString &srcDirPath, const QString &dstDirPath) const;
 };
